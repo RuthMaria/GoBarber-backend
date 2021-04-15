6bcb3f26b50541bee1d038bd0cc68f70b5b45174
RuthMaria <ruth-15rms@hotmail.com>
2021-04-14 23:07:33 -0300

    refactored code

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,34 +1,32 @@
 import Appointment from '../infra/typeorm/entities/Appointments'
 import {startOfHour} from 'date-fns' //  startOdHour pega uma data e zera os segundos e minutos.
-import AppointmentRepository from '../repositories/AppointmentsRepository'
-import {getCustomRepository} from 'typeorm'
 import AppError from '@shared/errors/AppError'
+import IAppointmentRepository from '../repositories/IAppointmentsRepository';
 
-interface Request {
+interface IRequest {
     provider_id: string
     date: Date
 }
 
 class CreateAppointmentService {
 
-    public async execute({ provider_id, date }: Request): Promise<Appointment >{
+    constructor(private appointmentsRepository: IAppointmentRepository){}
 
-        const appointmentsRepository = getCustomRepository(AppointmentRepository)
+    public async execute({ provider_id, date }: IRequest): Promise<Appointment >{
 
         const appointmentDate = startOfHour(date)
 
-        const findAppointmentsInSameDate = await appointmentsRepository.findByDate(appointmentDate)
+        const findAppointmentsInSameDate = await this.appointmentsRepository.findByDate(appointmentDate)
 
         if(findAppointmentsInSameDate){
             throw new AppError('This appointments is already booked')
         }
 
-        const appointment = appointmentsRepository.create({
+        const appointment = await this.appointmentsRepository.create({
             provider_id,
             date: appointmentDate
         })
 
-        await appointmentsRepository.save(appointment)
         return appointment
     }
 }

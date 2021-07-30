4793ce002a1a554e5d71c5e44d989be6af3bd15d
RuthMaria <ruth-15rms@hotmail.com>
2021-07-30 01:00:14 -0300

    testing user creation

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import CreateUserService from "./CreateUserService";
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+import AppError from '@shared/errors/AppError';
+
+describe("CreateUser", () => {
+    it("should be able to create a new user", async () => {
+        const fakeUsersRepository = new FakeUsersRepository()
+        const createUser = new CreateUserService(fakeUsersRepository)
+
+        const user = await createUser.execute({
+            name: 'Ruth Maria',
+            email: 'ruthmariia01@gmail.com',
+            password: '123456'
+        })
+
+        expect(user).toHaveProperty('id')
+    })
+
+    it("should not be able to create a new user with same email from another", async () => {
+        const fakeUsersRepository = new FakeUsersRepository()
+        const createUser = new CreateUserService(fakeUsersRepository)
+
+        const user = await createUser.execute({
+            name: 'Ruth Maria',
+            email: 'ruthmariia01@gmail.com',
+            password: '123456'
+        })
+
+        expect(
+            createUser.execute({
+                name: 'Ruth Maria',
+                email: 'ruthmariia01@gmail.com',
+                password: '123456'
+            })
+        ).rejects.toBeInstanceOf(AppError)
+    })
+
+})

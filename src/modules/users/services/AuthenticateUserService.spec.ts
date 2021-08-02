7615d95cb18f639315da13f4a4f8cb0ab5e61288
RuthMaria <ruth-15rms@hotmail.com>
2021-08-02 00:09:27 -0300

    testing wrong password

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -13,7 +13,7 @@ describe("AuthenticateUser", () => {
         const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
         const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
 
-        await createUser.execute({
+        const user = await createUser.execute({
             name: 'Ruth Maria',
             email: 'ruthmariia01@gmail.com',
             password: '123456'
@@ -25,5 +25,41 @@ describe("AuthenticateUser", () => {
         })
 
         expect(response).toHaveProperty('token')
+        expect(response.user).toEqual(user)
+    })
+
+    it("should not be able to authenticate with non existing user", async () => {
+        const fakeUsersRepository = new FakeUsersRepository()
+        const fakeHashProvider = new FakeHashProvider()
+
+        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
+
+        expect(
+            authenticateUser.execute({
+                email: 'ruthmariia01@gmail.com',
+                password: '123456'
+            })
+        ).rejects.toBeInstanceOf(AppError)
+    })
+
+    it("should not be able to authenticate with wrong password", async () => {
+        const fakeUsersRepository = new FakeUsersRepository()
+        const fakeHashProvider = new FakeHashProvider()
+
+        const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
+        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
+
+        await createUser.execute({
+            name: 'Ruth Maria',
+            email: 'ruthmariia01@gmail.com',
+            password: '123456'
+        })
+
+        expect(
+            authenticateUser.execute({
+                email: 'ruthmariia01@gmail.com',
+                password: 'wrong-password'
+            })
+        ).rejects.toBeInstanceOf(AppError)
     })
 })

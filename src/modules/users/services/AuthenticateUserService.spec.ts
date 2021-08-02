43bffba1f7d32899d4ecacc99852d0392273773e
RuthMaria <ruth-15rms@hotmail.com>
2021-08-02 10:36:18 -0300

    testing wrong user

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -62,4 +62,18 @@ describe("AuthenticateUser", () => {
             })
         ).rejects.toBeInstanceOf(AppError)
     })
+
+    it("should not be able to authenticate with non existing user", async () => {
+        const fakeUsersRepository = new FakeUsersRepository()
+        const fakeHashProvider = new FakeHashProvider()
+
+        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
+
+        expect(
+            authenticateUser.execute({
+                email: 'ruthmariia01@gmail.com',
+                password: '123456'
+            })
+        ).rejects.toBeInstanceOf(AppError)
+    })
 })
